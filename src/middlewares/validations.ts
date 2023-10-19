15dd4fa4b265d251c1bad1c88b87430bbdae75c0
KovOlga <switch.to.olga@yandex.ru>
2023-10-19 13:07:41 +0300

    deleted unknow: true in validations

--- src/middlewares/validations.ts
+++ src/middlewares/validations.ts
@@ -20,89 +20,81 @@ export const validateObjectId = celebrate({
 });
 
 export const validateUserProfile = celebrate({
-  body: Joi.object()
-    .keys({
-      // eslint-disable-next-line newline-per-chained-call
-      name: Joi.string().min(2).max(30).messages({
-        "string.min": 'Минимальная длина поля "name" - 2',
-        "string.max": 'Максимальная длина поля "name" - 30',
-        "string.empty": 'Поле "name" должно быть заполнено',
+  body: Joi.object().keys({
+    // eslint-disable-next-line newline-per-chained-call
+    name: Joi.string().min(2).max(30).messages({
+      "string.min": 'Минимальная длина поля "name" - 2',
+      "string.max": 'Максимальная длина поля "name" - 30',
+      "string.empty": 'Поле "name" должно быть заполнено',
+    }),
+    // eslint-disable-next-line newline-per-chained-call
+    about: Joi.string().min(2).max(200).messages({
+      "string.min": 'Минимальная длина поля "about" - 2',
+      "string.max": 'Максимальная длина поля "about" - 200',
+      "string.empty": 'Поле "about" должно быть заполнено',
+    }),
+    avatar: Joi.string()
+      .pattern(urlRegex)
+      .message("Невалидный url-адрес")
+      .messages({
+        "string.empty": 'Поле "avatar" должно быть заполнено',
       }),
-      // eslint-disable-next-line newline-per-chained-call
-      about: Joi.string().min(2).max(200).messages({
-        "string.min": 'Минимальная длина поля "about" - 2',
-        "string.max": 'Максимальная длина поля "about" - 200',
-        "string.empty": 'Поле "about" должно быть заполнено',
+    email: Joi.string()
+      .required()
+      .email()
+      .message('В поле "email" необходимо вставить email')
+      .messages({
+        "string.empty": 'Поле "email" должно быть заполнено',
       }),
-      avatar: Joi.string()
-        .pattern(urlRegex)
-        .message("Невалидный url-адрес")
-        .messages({
-          "string.empty": 'Поле "avatar" должно быть заполнено',
-        }),
-      email: Joi.string()
-        .required()
-        .email()
-        .message('В поле "email" необходимо вставить email')
-        .messages({
-          "string.empty": 'Поле "email" должно быть заполнено',
-        }),
-      password: Joi.string().required().messages({
-        "string.empty": 'Поле "пароль" должно быть заполнено',
-      }),
-    })
-    .unknown(true),
+    password: Joi.string().required().messages({
+      "string.empty": 'Поле "пароль" должно быть заполнено',
+    }),
+  }),
 });
 
 export const validateUserData = celebrate({
-  body: Joi.object()
-    .keys({
-      // eslint-disable-next-line newline-per-chained-call
-      name: Joi.string().required().min(2).max(30).messages({
-        "string.min": 'Минимальная длина поля "name" - 2',
-        "string.max": 'Максимальная длина поля "name" - 30',
-        "string.empty": 'Поле "name" должно быть заполнено',
-      }),
-      // eslint-disable-next-line newline-per-chained-call
-      about: Joi.string().required().min(2).max(200).messages({
-        "string.min": 'Минимальная длина поля "about" - 2',
-        "string.max": 'Максимальная длина поля "about" - 200',
-        "string.empty": 'Поле "about" должно быть заполнено',
-      }),
-    })
-    .unknown(true),
+  body: Joi.object().keys({
+    // eslint-disable-next-line newline-per-chained-call
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'Минимальная длина поля "name" - 2',
+      "string.max": 'Максимальная длина поля "name" - 30',
+      "string.empty": 'Поле "name" должно быть заполнено',
+    }),
+    // eslint-disable-next-line newline-per-chained-call
+    about: Joi.string().required().min(2).max(200).messages({
+      "string.min": 'Минимальная длина поля "about" - 2',
+      "string.max": 'Максимальная длина поля "about" - 200',
+      "string.empty": 'Поле "about" должно быть заполнено',
+    }),
+  }),
 });
 
 export const validateAvatar = celebrate({
-  body: Joi.object()
-    .keys({
-      avatar: Joi.string()
-        .required()
-        .pattern(urlRegex)
-        .message("Невалидный url-адрес")
-        .messages({
-          "string.empty": 'Поле "avatar" должно быть заполнено',
-        }),
-    })
-    .unknown(true),
+  body: Joi.object().keys({
+    avatar: Joi.string()
+      .required()
+      .pattern(urlRegex)
+      .message("Невалидный url-адрес")
+      .messages({
+        "string.empty": 'Поле "avatar" должно быть заполнено',
+      }),
+  }),
 });
 
 export const validateNewCard = celebrate({
-  body: Joi.object()
-    .keys({
-      link: Joi.string()
-        .required()
-        .pattern(urlRegex)
-        .message("Необходимо вставить ссылку")
-        .messages({
-          "string.empty": "Поле с ссылкой должно быть заполнено",
-        }),
-      // eslint-disable-next-line newline-per-chained-call
-      name: Joi.string().required().min(2).max(30).messages({
-        "string.min": 'Минимальная длина поля "name" - 2',
-        "string.max": 'Максимальная длина поля "name" - 30',
-        "string.empty": 'Поле "name" должно быть заполнено',
+  body: Joi.object().keys({
+    link: Joi.string()
+      .required()
+      .pattern(urlRegex)
+      .message("Необходимо вставить ссылку")
+      .messages({
+        "string.empty": "Поле с ссылкой должно быть заполнено",
       }),
-    })
-    .unknown(true),
+    // eslint-disable-next-line newline-per-chained-call
+    name: Joi.string().required().min(2).max(30).messages({
+      "string.min": 'Минимальная длина поля "name" - 2',
+      "string.max": 'Максимальная длина поля "name" - 30',
+      "string.empty": 'Поле "name" должно быть заполнено',
+    }),
+  }),
 });
