db4dc2faed6b6aefe7725d7b26d60a42ded71107
KovOlga <switch.to.olga@yandex.ru>
2023-12-13 13:09:17 +0300

    enabled cors policy

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { errors } from "celebrate";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import errorHandler from "./middlewares/error-handler";
 import { PORT, DB_ADDRESS } from "./config";
 import { requestLogger, errorLogger } from "./middlewares/logger";
 import routes from "./routes";
 
 const app = express();
+app.use(cors());
 mongoose.connect(DB_ADDRESS);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
